581bc5b25cea2bf9d2697879191a4c202d8ad2d3
Vadim Khamatov <74698624+vrkhamatov@users.noreply.github.com>
2023-03-24 18:28:23 +0300

    Added write in txt file

--- src/main/java/com/example/src/FileMaker.java
+++ src/main/java/com/example/src/FileMaker.java
@@ -1,38 +1,53 @@
 package com.example.src;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class FileMaker {
 
-    public void output(HashMap<String, List<UserRent>> userRentHashMap) {
+
+
+    public void makeTxtFile(String output){
+
+    }
+
+    public void output(HashMap<String, List<UserRent>> userRentHashMap) throws IOException {
 
 
         Set<String> setOfKeys = userRentHashMap.keySet();
         Iterator iterator = setOfKeys.iterator();
         List<UserRent> userRentList = new ArrayList<>();
+        int q = 0;
         while (iterator.hasNext()) {
+            q = q +1;
             userRentList = userRentHashMap.get(iterator.next());
             //Collections.sort(userRentList, Comparator.comparing(UserRent::getStartTime));
             Double summaryCost = 0.0;
+            File file = new File("reports\\" + q + ".txt");
+            FileWriter writer = null;
+            writer = new FileWriter(file);
             for (int i = 0; i < userRentList.size(); i++) {
                 if (i == 0) {
-                    System.out.println("Tariff index " + userRentList.get(i).getTariffType());
-                    System.out.println("----------------------------------------------------------------------------");
-                    System.out.println("Report for phone number " + userRentList.get(i).getNumber() + ":");
-                    System.out.println("| Call Type |   Start Time        |     End Time        | Duration | Cost  |");
-                    System.out.println("----------------------------------------------------------------------------");
+                    writer.write("Tariff index " + userRentList.get(i).getTariffType() + "\n");
+                    writer.write("----------------------------------------------------------------------------"+ "\n");
+                    writer.write("Report for phone number " + userRentList.get(i).getNumber() + ":"+ "\n");
+                    writer.write("| Call Type |   Start Time        |     End Time        | Duration | Cost  |"+ "\n");
+                    writer.write("----------------------------------------------------------------------------"+ "\n");
                 }
-                System.out.println("|     " + userRentList.get(i).getCallType() + "    | " + userRentList.get(i).getStartTimeFormatted() + " | " + userRentList.get(i).getEndTimeFormatted() + " | " + userRentList.get(i).getDurationString() + " |  " + userRentList.get(i).getCost());
+                writer.write("|     " + userRentList.get(i).getCallType() + "    | " + userRentList.get(i).getStartTimeFormatted() + " | " + userRentList.get(i).getEndTimeFormatted() + " | " + userRentList.get(i).getDurationString() + " |  " + userRentList.get(i).getCost()+ "\n");
                 if (i == userRentList.size()-1)
-                summaryCost += userRentList.get(i).getCost()+100;
+                    summaryCost += userRentList.get(i).getCost()+100;
                 else
                     summaryCost += userRentList.get(i).getCost();
             }
-            System.out.println("----------------------------------------------------------------------------");
+            writer.write("----------------------------------------------------------------------------"+ "\n");
 
-            System.out.println("|                                           Total Cost: |     " + summaryCost + " |");
-            System.out.println("----------------------------------------------------------------------------");
-            System.out.println("   ");
+            writer.write("|                                           Total Cost: |     " + summaryCost + " |"+ "\n");
+            writer.write("----------------------------------------------------------------------------"+ "\n");
+            writer.write("   "+ "\n");
+            writer.close();
         }
 
 
